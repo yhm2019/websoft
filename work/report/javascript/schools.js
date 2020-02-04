cea829d50d6c1e218569565cb06aeb507299f802
Hanmeng Yuan <931780003@qq.com>
2020-02-04 15:37:37 +0100

    a03

--- work/report/javascript/schools.js
+++ work/report/javascript/schools.js
@@ -12,9 +12,9 @@
         .then((response) => {
             return response.json();
         })
-        .then((myJson) => {
-            console.log(myJson);
-        });
+        // .then((myJson) => {
+        //     console.log(myJson);
+        // });
     })
    
    
