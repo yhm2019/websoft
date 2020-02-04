79074e4a1e5b4a73fa1175cfd9bd65d9915f0743
Hanmeng Yuan <931780003@qq.com>
2020-02-04 11:58:19 +0100

    a03

---
+++ work/report/javascript/schools.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+(function () {
+    'use strict';
+
+    let url;
+
+    url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
+//    url = "data/1081.json";
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+    url = "https://rem.dbwebb.se/api/users";
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+    console.log('Sandbox is ready!');
+})();
