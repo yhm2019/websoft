8d1eef29d154418d6b21b916287a78c721ce2658
Hanmeng Yuan <931780003@qq.com>
2020-02-04 13:45:07 +0100

    a03

--- work/report/javascript/schools.js
+++ work/report/javascript/schools.js
@@ -1,10 +1,10 @@
 // JavaScript Document
-(function () {
+document.getElemrntById("fetch").addEventListener("click",function () {
     'use strict';
  let url;
 	
-	url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
-//    url = "data/1081.json";
+//	url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
+    url = "data/1081.json";
     fetch(url)
         .then((response) => {
             return response.json();
@@ -22,7 +22,7 @@
 //        .then((myJson) => {
 //            console.log(myJson);
 //        });
-
-    console.log('Sandbox is ready!');
-	document.getElementById("content").innerHTML = "hi";
-})();
+//
+//    console.log('Sandbox is ready!');
+	document.getElementById("content").innerHTML;
+};
