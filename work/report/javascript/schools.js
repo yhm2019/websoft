63c82112796127a650ea75bad228df0c4e8f523c
Hanmeng Yuan <931780003@qq.com>
2020-02-04 13:29:02 +0100

    a03

--- work/report/javascript/schools.js
+++ work/report/javascript/schools.js
@@ -3,8 +3,8 @@
     'use strict';
  let url;
 	
-	//url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
-    url = "data/1081.json";
+	url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
+//    url = "data/1081.json";
     fetch(url)
         .then((response) => {
             return response.json();
@@ -14,14 +14,15 @@
         });
 
    
-	url = "https://rem.dbwebb.se/api/users";
-	fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((myJson) => {
-            console.log(myJson);
-        });
+//	url = "https://rem.dbwebb.se/api/users";
+//	fetch(url)
+//        .then((response) => {
+//            return response.json();
+//        })
+//        .then((myJson) => {
+//            console.log(myJson);
+//        });
 
     console.log('Sandbox is ready!');
+	document.getElementById("content").innerHTML = "hi";
 })();
