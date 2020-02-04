39ed8812918d6cf4d3d4ab32331966674bc3c06a
Hanmeng Yuan <931780003@qq.com>
2020-02-04 17:35:24 +0100

    a03

--- work/report/javascript/rabbit.js
+++ work/report/javascript/rabbit.js
@@ -5,12 +5,16 @@
 	var element = document.getElementById("rabbit");
 	element.addEventListener("click",function(){
         element.style.left = element.offsetLeft + 20 + "px";
-        element.innerHTML = "<p>" + element.offsetLeft + "</p>";
+        element.innerHTML = "<p>" + "Rabbit will disappear in 6 seconds" + "</p>";
 
          console.log("element.offsetLeft");
 		console.log("Rabbit clicked");
 	});
 
-    
-	console.log("Rabbit ready");
+    var time = 6000;
+    function disappear() {
+        element.style.visibility = "hidden";
+    }
+    window.setInterval(disappear, time);
+	// console.log("Rabbit ready");
 })();
--- work/report/javascript/schools.js
+++ work/report/javascript/schools.js
@@ -1,13 +1,13 @@
 // JavaScript Document
 'use strict';
 (function () {
-    var btn = document.getElementById("fetch");
-    var element = document.getElementById("content");
+    // var btn = document.getElementById("fetch");
+    // var element = document.getElementById("content");
     let url;
 	
     url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
-   // url = "data/1081.json";
-    btn.addEventListener("click",function(){
+    // url = "data/1081.json";
+    // btn.addEventListener("click",function(){
          fetch(url)
         .then((response) => {
             return response.json();
@@ -15,8 +15,9 @@
         .then((myJson) => {
             console.log(myJson);
         });
-    })
-})
+
+    console.log('Sandbox MEGA is ready!');
+})();
    
    
 //	url = "https://rem.dbwebb.se/api/users";
