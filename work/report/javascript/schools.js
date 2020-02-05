ea0e52016cfb07206266f7cb0f84cc5f8ccbe424
Hanmeng Yuan <931780003@qq.com>
2020-02-05 18:46:08 +0100

    a03

--- work/report/javascript/schools.js
+++ work/report/javascript/schools.js
@@ -1,40 +1,18 @@
 // JavaScript Document
-'use strict';
 (function () {
-    var btn = document.getElementById("fetch");
-    var element = document.getElementById("data");
+    'use strict';
+
     let url;
-    // url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
+
+    //url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
     url = "data/1081.json";
-    btn.addEventListener("click",function(){
-         fetch(url)
+    fetch(url)
         .then((response) => {
             return response.json();
         })
         .then((myJson) => {
             console.log(myJson);
-            myJson.Skolenheter = myJson.Skolenheter.filter(function(a){
-                return a.Skolenhetskod === "11842600";
-            })
         });
-        element.innerHTML = JSON.stringify(myJson.Skolenheter);
 
-    });
-
-    console.log('Sandbox MEGA is ready!');
+    console.log('Sandbox is ready!');
 })();
-   
-   
-//  url = "https://rem.dbwebb.se/api/users";
-//  fetch(url)
-//        .then((response) => {
-//            return response.json();
-//        })
-//        .then((myJson) => {
-//            console.log(myJson);
-//        });
-//
-//    console.log('Sandbox is ready!');
-//  document.getElementById("content").innerHTML;
-// })();
-//document.getElemrntById("fetch").addEventListener
