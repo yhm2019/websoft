4be8bfb82f620b30371b83b5fa50af5376f33a43
Hanmeng Yuan <931780003@qq.com>
2020-02-06 18:27:27 +0100

    a03

--- work/report/javascript/schools.js
+++ work/report/javascript/schools.js
@@ -3,7 +3,9 @@
     'use strict';
 
     let url;
-
+    var school;
+    var btn = document.getElementById("fetch");
+    var element = document.getElementById("data")
     //url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
     url = "data/1081.json";
     fetch(url)
@@ -11,8 +13,19 @@
             return response.json();
         })
         .then((myJson) => {
+            school = myJson;
             console.log(myJson);
-        });
+            console.log("1");
+            });
 
+    btn.addEventListener("click",function(a){
+        schools.Skolenheter = schools.Skolenheter.filter(function(a){
+            return a.Skolenhetskod === "11842600";
+        })
+        console.log(schools.Skolenheter);
+    });
+    console.log = JSON.stringify(schools.Skolenheter,null,2);
+    data.innerHTML = JSON.stringify(schools.Skolenheter,null,2);
     console.log('Sandbox is ready!');
 })();
+
