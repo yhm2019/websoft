2baad486b13d5cd4e6d0d555c8f67a4598860845
Hanmeng Yuan <931780003@qq.com>
2020-02-22 21:22:43 +0100

    a03

--- work/report/javascript/schools.js
+++ work/report/javascript/schools.js
@@ -25,11 +25,11 @@
                 var c1 = row.insertCell(0);
                 c1.innerHTML = myJson[i].Skolenhetskod;
                 var c2 = row.insertCell(1);
-                c1.innerHTML = myJson[i].Skolenhetsnamn;
+                c2.innerHTML = myJson[i].Skolenhetsnamn;
                 var c3 = row.insertCell(2);
-                c1.innerHTML = myJson[i].Kommunkod;
+                c3.innerHTML = myJson[i].Kommunkod;
                 var c4 = row.insertCell(3);
-                c1.innerHTML = myJson[i].PeOrgNr;
+                c4.innerHTML = myJson[i].PeOrgNr;
             }
             // data.innerHTML = JSON.stringify(myJson, null, 4);
             //tansfer string to js object
